beddeffa3393ca84712dbf9ab88b238e0680b452
majestyuwa <ifymaj@gmail.com>
2019-07-23 18:39:44 -0400

    Setting up MongoDB and creating first model

--- Atlas/App.tsx
+++ Atlas/App.tsx
@@ -1,6 +1,25 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+var express = require('express')
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+
+// connecting to database using MongoDB
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://majesty_uwagerikpe:atlasadmin2019@cluster-atlas-2jaxm.azure.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+// database requirements
+var User = require('./models/User.tsx');
 export default function App() {
   return (
     <View style={styles.container}>
@@ -8,6 +27,7 @@ export default function App() {
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
